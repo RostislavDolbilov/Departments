e8e780f60fe6b88d66cca2baa84bbfd2f9f256e6
rostislavdolbilov <rostislavdolbilov@gmail.com>
2021-09-20 17:10:48 +0300

    ф

--- src/main/java/epam/rostislav/config/AppSpringConfig.java
+++
@@ -1,8 +0,0 @@
-package epam.rostislav.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "epam.rostislav")
-public class AppSpringConfig {}
